a2bb7829bedfabe54747bb4e00522c6b62a763a1
juanputrantoo <43021047+juanputrantoo@users.noreply.github.com>
2018-10-16 12:38:47 +0700

    perubahan pada file ts

--- home/home.ts
+++ home/home.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { SignupPage } from '../signup/signup';
-import { TabtabPage } from '../tabtab/tabtab';
-import { TabpedagangPage } from '../tabpedagang/tabpedagang';
+import { TabsPage } from '../tabs/tabs';
+import { LoadingController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { Storage } from '@ionic/storage';
+import { HomepdgPage } from '../homepdg/homepdg';
 
 
 @Component({
@@ -13,11 +17,72 @@ export class HomePage {
 
   SignUpButton: any;
   LogInButton: any;
-  LogInPdgButton: any;
-  constructor(public navCtrl: NavController) {
+  username: string;
+  password: string;
+  message: string;
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController, public http: HttpClient, public storage: Storage) {
     this.SignUpButton = SignupPage;
-    this.LogInButton= TabtabPage;
-    this.LogInPdgButton= TabpedagangPage;
+    this.LogInButton = HomepdgPage;
+  
   }
 
+  login(){
+    let lg : Observable<any> = this.http.post('http://mamafood.com/api/login',{
+      username : this.username,
+      password : this.password
+    });
+    lg.subscribe((data) => {
+      if(data.token){
+        this.storage.set('token',data.token);
+        let loader = this.loadingCtrl.create({
+          spinner: "dots",
+          content: "Makanan Mama Menunggu...",
+          duration:2000
+        });
+        loader.present();
+        this.http.get('http://mamafood.com/api/token',{
+          headers : {
+            'Authorization' :   data.token
+          }
+        }).subscribe((user) => {
+          if(parseInt(user['role']) === 1){
+            this.navCtrl.push(HomepdgPage);
+          }else{
+            this.navCtrl.push(TabsPage);
+          }
+         
+        });
+      }else{
+        this.message = data.message;
+      }
+    })
+  }
+
+
+  // test(){
+  //   let lg : Observable<any> = this.http.get('http://172.20.10.4/mamafood2/api/test',{
+  //     headers : {
+  //       Authorization : 'juanputranto.e94b97ff5fcb5f433a4587f577ddf8419e4ca5ad851e17719381761d539ae0dc'
+  //     }
+  //   });
+  //   lg.subscribe((data) => {
+  //     console.log(data);
+  //   })
+    
+  // }
+
+  // loading(){
+  // 	let loader = this.loadingCtrl.create({
+  // 		spinner: "dots",
+  // 		content: "Makanan Mama Menunggu...",
+  // 		duration:3000
+  // 	});
+  //   loader.present();
+    
+  // 	this.navCtrl.push(TabsPage);
+  // }
+
+
+
+  
 }
