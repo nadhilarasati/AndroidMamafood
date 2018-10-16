505ba898f59e3158e1cb29a78cd63481bdea37b8
juanputrantoo <43021047+juanputrantoo@users.noreply.github.com>
2018-10-16 13:07:33 +0700

    fungsi regmer

--- regmer/regmer.ts
+++ regmer/regmer.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController, ToastController, Platform, LoadingController, Loading } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+
+import { File } from '@ionic-native/file';
+import { Transfer, TransferObject } from '@ionic-native/transfer';
+import { FilePath } from '@ionic-native/file-path';
+import { Camera } from '@ionic-native/camera';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the RegmerPage page.
@@ -8,18 +15,141 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
+declare var cordova: any;
+
 @IonicPage()
 @Component({
   selector: 'page-regmer',
   templateUrl: 'regmer.html',
 })
 export class RegmerPage {
+  lastImage: string = null;
+  loading: Loading;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  deskripsiMenu: any;
+  namaMenu: any;
+  hargaMenu: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private camera: Camera, private transfer: Transfer, private file: File, private filePath: FilePath, public actionSheetCtrl: ActionSheetController, public toastCtrl: ToastController, public platform: Platform, public loadingCtrl: LoadingController, public http: HttpClient) {
+  }
+
+  postPedagang() {
+    let form = {
+      deskripsiMenu : this.deskripsiMenu,
+      namaMenu : this.namaMenu,
+      hargaMenu : this.hargaMenu
+    };
+    this.http.post('http://172.20.10.4/mamafood2/api/signup', form,{
+      headers : {
+        'Content-Type' : 'application/json'
+      }
+    }).subscribe(res => {
+      this.navCtrl.push(HomePage);
+    })
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad RegmerPage');
+    console.log('ionViewDidLoad SignupPage');
   }
 
+  public takePicture(sourceType) {
+    var options = {
+      quality: 100,
+      sourceType: sourceType,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    };
+    this.camera.getPicture(options).then((imagePath) => {
+      if (this.platform.is('android') && sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
+        this.filePath.resolveNativePath(imagePath)
+          .then(filePath => {
+            let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
+            let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
+            this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+          });
+      } else {
+        var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+        var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+        this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+      }
+    }, (err) => {
+      console.log(err);
+      this.presentToast('Error while selecting image.');
+    });
+  }
+    public presentActionSheet() {
+      let actionSheet = this.actionSheetCtrl.create({
+        title: 'Gallery',
+        buttons: [
+          {
+            text: 'Photos',
+            handler: () => {
+              this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+            }
+          },
+          {
+            text: 'Camera',
+            handler: () => {
+              this.takePicture(this.camera.PictureSourceType.CAMERA);
+            }
+          },
+          {
+            text: 'Cancel',
+            role: 'cancel'
+          }
+        ]
+      });
+      actionSheet.present();
+    }
+  private createFileName() {
+    var d = new Date(),
+    n = d.getTime(),
+    newFileName =  n + ".jpg";
+    return newFileName;
+  }
+  private copyFileToLocalDir(namePath, currentName, newFileName) {
+    this.file.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
+      this.lastImage = newFileName;
+    }, error => {
+      this.presentToast('Error while storing file.');
+    });
+  }
+  private presentToast(text) {
+    let toast = this.toastCtrl.create({
+      message: text,
+      duration: 3000,
+      position: 'top'
+    });
+    toast.present();
+  }
+  public pathForImage(img) {
+    if (img === null) {
+      return '';
+    } else {
+      return cordova.file.dataDirectory + img;
+    }
+  }
+  public uploadImage() {
+    var url = "Give your API Path";
+    var targetPath = this.pathForImage(this.lastImage);
+    var filename = this.lastImage;
+    var options = {
+      fileKey: "file",
+      fileName: filename,
+      chunkedMode: false,
+      mimeType: "multipart/form-data",
+      params : {'fileName': filename}
+    };
+    const fileTransfer: TransferObject = this.transfer.create();
+    this.loading = this.loadingCtrl.create({
+      content: 'Uploading...',
+    });
+    this.loading.present();
+    fileTransfer.upload(targetPath, url, options).then(data => {
+      this.loading.dismissAll()
+      this.presentToast('Image succesful uploaded.');
+    }, err => {
+      this.loading.dismissAll()
+      this.presentToast('Error while uploading file.');
+    });
+  }
 }
